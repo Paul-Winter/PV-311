9c10852a94705e8de34459dd5ddb975b25f16236
Paul Winter <zimin.pavel.v@gmail.com>
2024-07-31 19:28:59 +0300

    add methodic and conspect

--- Exams/C++ OOP/Гладырев/hangman14.cpp
+++ Exams/C++ OOP/Гладырев/hangman14.cpp
@@ -7,21 +7,18 @@
 using namespace std;
 
 bool isCorrectGuess(const string& secretWord, const vector<bool>& guessedLetters) {
-    for (size_t i = 0; i < secretWord.size(); ++i)
-    {
+    for (size_t i = 0; i < secretWord.size(); ++i) {
         if (!guessedLetters[i])
-        {
-            return false;
+        { 
+            return false; 
         }
     }
-    return true;
+    return true; 
 }
 
 
-void drawHangman(int incorrectGuesses)
-{
-    if (incorrectGuesses == 0)
-    {
+void drawHangman(int incorrectGuesses) {
+    if (incorrectGuesses == 0) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |" << endl;
@@ -30,8 +27,7 @@ void drawHangman(int incorrectGuesses)
         cout << " |" << endl;
         cout << "_|________" << endl;
     }
-    else if (incorrectGuesses == 1)
-    {
+    else if (incorrectGuesses == 1) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |        O" << endl;
@@ -40,8 +36,7 @@ void drawHangman(int incorrectGuesses)
         cout << " |" << endl;
         cout << "_|________" << endl;
     }
-    else if (incorrectGuesses == 2)
-    {
+    else if (incorrectGuesses == 2) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |        O" << endl;
@@ -50,8 +45,7 @@ void drawHangman(int incorrectGuesses)
         cout << " |" << endl;
         cout << "_|________" << endl;
     }
-    else if (incorrectGuesses == 3)
-    {
+    else if (incorrectGuesses == 3) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |        O" << endl;
@@ -60,8 +54,7 @@ void drawHangman(int incorrectGuesses)
         cout << " |" << endl;
         cout << "_|________" << endl;
     }
-    else if (incorrectGuesses == 4)
-    {
+    else if (incorrectGuesses == 4) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |        O" << endl;
@@ -70,8 +63,7 @@ void drawHangman(int incorrectGuesses)
         cout << " |" << endl;
         cout << "_|________" << endl;
     }
-    else if (incorrectGuesses == 5)
-    {
+    else if (incorrectGuesses == 5) {
         cout << "  ________" << endl;
         cout << " |        |" << endl;
         cout << " |        O" << endl;
@@ -92,33 +84,32 @@ void drawHangman(int incorrectGuesses)
 }
 
 int main() {
-    vector<string> words;
+    vector<string> words; 
     string word;
-    ifstream inputFile("words.txt");
+    ifstream inputFile("words.txt"); 
     while (getline(inputFile, word)) {
-        if (!word.empty() && word[word.size() - 1] == '\r')
-        {
+        if (!word.empty() && word[word.size() - 1] == '\r') { 
             word = word.substr(0, word.size() - 1);
         }
-        words.push_back(word);
+        words.push_back(word); 
     }
 
-    inputFile.close();
-    srand(static_cast<unsigned int>(time(nullptr)));
-    string secretWord = words[rand() % words.size()];
+    inputFile.close();  
+    srand(static_cast<unsigned int>(time(nullptr))); 
+    string secretWord = words[rand() % words.size()]; 
     vector<bool> guessedLetters(secretWord.size(), false);
-    int attemptsLeft = 6;
+    int attemptsLeft = 6; 
     cout << "Hangman Game" << endl;
 
-    while (attemptsLeft > 0) {
-
+    while (attemptsLeft > 0) { 
+        
         cout << "Secret Word: ";
         for (size_t i = 0; i < secretWord.size(); ++i) {
             if (guessedLetters[i]) {
-                cout << secretWord[i] << " ";
+                cout << secretWord[i] << " "; 
             }
             else {
-                cout << "_ ";
+                cout << "_ "; 
             }
         }
         cout << endl;
@@ -126,29 +117,30 @@ int main() {
         cout << "Guess a letter: ";
         cin >> inputChar;
         bool letterFound = false;
-        for (size_t i = 0; i < secretWord.size(); ++i) {
+        for (size_t i = 0; i < secretWord.size(); ++i) 
+        {
             if (secretWord[i] == inputChar) {
                 guessedLetters[i] = true;
                 letterFound = true;
             }
         }
 
-        if (!letterFound)
+        if (!letterFound) 
         {
-            --attemptsLeft;
+            --attemptsLeft; 
             cout << "Incorrect guess. Attempts left: " << attemptsLeft << endl;
         }
         drawHangman(6 - attemptsLeft);
-        if (isCorrectGuess(secretWord, guessedLetters))
-        {
+        if (isCorrectGuess(secretWord, guessedLetters)) 
+        { 
             cout << "Congratulations! You have won the game. The word was:" << secretWord << endl;
             break;
         }
     }
 
-    if (attemptsLeft <= 0)
-    {
-        cout << "Game Over! The word was: " << secretWord << endl;
+    if (attemptsLeft <= 0) 
+    { 
+        cout << "Game Over! The word was: " << secretWord << endl; 
     }
 
     return 0;
